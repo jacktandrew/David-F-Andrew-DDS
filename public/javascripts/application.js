359eb616f04f23241cad74f0dc41a2a1e75e07ce
Jack Andrew <jacktandrew@gmail.com>
2011-10-11 15:36:25 -0700

    divide index.html into .css .js & .erb

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,47 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+  var map;
+  var gdir;
+  var geocoder = null;
+  var addressMarker;
+
+  function initialize() {
+    if (GBrowserIsCompatible()) {      
+      map = new GMap2(document.getElementById("map_canvas"));
+      gdir = new GDirections(map, document.getElementById("directions"));
+      GEvent.addListener(gdir, "load", onGDirectionsLoad);
+      GEvent.addListener(gdir, "error", handleErrors);
+
+      setDirections("Point Defiance", "2302 S Union", "en_US");
+    }
+  }
+
+  function setDirections(fromAddress, toAddress, locale) {
+    gdir.load("from: " + fromAddress + " to: " + toAddress,
+              { "locale": locale });
+  }
+
+  function handleErrors(){
+    if (gdir.getStatus().code == G_GEO_UNKNOWN_ADDRESS)
+      alert("No corresponding geographic location could be found for one of the specified addresses. This may be due to the fact that the address is relatively new, or it may be incorrect.\nError code: " + gdir.getStatus().code);
+  else if (gdir.getStatus().code == G_GEO_SERVER_ERROR)
+    alert("A geocoding or directions request could not be successfully processed, yet the exact reason for the failure is not known.\n Error code: " + gdir.getStatus().code);
+
+  else if (gdir.getStatus().code == G_GEO_MISSING_QUERY)
+    alert("The HTTP q parameter was either missing or had no value. For geocoder requests, this means that an empty address was specified as input. For directions requests, this means that no query was specified in the input.\n Error code: " + gdir.getStatus().code);
+
+  else if (gdir.getStatus().code == G_GEO_BAD_KEY)
+    alert("The given key is either invalid or does not match the domain for which it was given. \n Error code: " + gdir.getStatus().code);
+
+  else if (gdir.getStatus().code == G_GEO_BAD_REQUEST)
+    alert("A directions request could not be successfully parsed.\n Error code: " + gdir.getStatus().code);
+
+  else alert("An unknown error occurred.");
+  }
+
+function onGDirectionsLoad(){ 
+  // Use this function to access information about the latest load()
+  // results.
+
+  // e.g.
+  // document.getElementById("getStatus").innerHTML = gdir.getStatus().code;
+// and yada yada yada...
+}
